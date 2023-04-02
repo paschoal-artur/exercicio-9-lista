ea019d97a6bcb1df1d7518ea015743b49f548619
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-01 23:59:40 -0300

    added code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+#Receiving temperature from user
+temp = float(input("What's the fahrenheit temperature at your location? \n"))
 
+#Transforming into celsius
+celsius = ((temp - 32) / 9) * 5
+celsius = int(celsius)
+
+#Printing to the user
+print(f"The temperature in Celsius where you are is {celsius}°")
